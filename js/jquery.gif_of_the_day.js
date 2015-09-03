9aead271ec6ffae2469a858cf56cc99a80a5d5b3
mattscilipoti <matt.scilipoti@ga.co>
2015-09-03 09:33:51 -0400

    Step 3: $.getJSON (with fixed category)

--- js/jquery.gif_of_the_day.js
+++ js/jquery.gif_of_the_day.js
@@ -4,7 +4,28 @@
 (function(window, document, $) {
 
   $.fn.gifOfTheDay = function(){
-    this.text("The plugin will put the gif here.");
+    var self = this;
+    var category = "funny+cat";
+    var apiKey = "dc6zaTOxFJmzC"; // Note: this is the *public beta* key.  Use env var for personal keys.
+    var giphyUrl = "http://api.giphy.com/v1/gifs/search?limit=1&rating=pg&q=" + category + "&api_key=" + apiKey;
+    $.getJSON( giphyUrl).then(function(giphyResults){
+      var imageUrl = parseImageFromGiphy(giphyResults, 0); // we only requested one (limit=1)
+      appendImageTag(self, imageUrl, category);
+    });
+
+    return this; // for jQuery chaining
   };
+
+  function parseImageFromGiphy(giphyResults, imageIndex) {
+    var imageData = giphyResults.data[imageIndex];
+    var imageUrl = imageData.images.fixed_height.url; // because `.url` was https
+    return imageUrl;
+  }
+
+  function appendImageTag(container, imageUrl, category){
+    var $imageTag = $("<img>");
+    $imageTag.attr('src', imageUrl).attr('alt', category); //note: chaining!
+    $(container).append($imageTag);
+  }
 
 }(window, document, window.jQuery)); // The global window, document, and jQuery objects are passed into the anonymous function
